f199bcae8983782c10052ed346c29467a55c9ec1
bc <123@qq.com>
2019-07-01 16:56:13 +0800

    Initial commit

---
+++ student_web/src/main/java/com/qf/student_web/StudentWebApplication.java
@@ -0,0 +1,13 @@
+package com.qf.student_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentWebApplication.class, args);
+    }
+
+}
---
+++ student_web/src/test/java/com/qf/student_web/StudentWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.qf.student_web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StudentWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
